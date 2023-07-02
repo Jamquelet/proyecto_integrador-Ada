2d940d2f64a6eeb23770f3bdac2a4af0ca6f5151
Jose Maya <jamquelet@hotmail.com>
2023-07-02 17:54:17 -0500

    Bienvenida del jugador

---
+++ main.py
@@ -0,0 +1,8 @@
+name_player = input("Escribe el nombre para tú jugador: ")
+print("Bienvenido", name_player, "diviertete en esta nueva aventura")
+
+dificultad = input("En que nivel quieres empezar: -Dificil -Medio -Bajo --> ")
+print("Tú modo de juego sera:", dificultad)
+
+mundo = input("En que mundo te sientes mas comodo: -Ogrimmar -Entrañas -Ventormenta --> ")
+print("Te esperan grandes aventuras en", mundo, ",", name_player, "se valiente y asegurate de encontrar la armadura dorada ")
